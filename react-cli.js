566d22510fe2df64c7aace8a4512daa500635dcb
Tyler Schum <tschumdev@gmail.com>
2019-02-13 15:45:35 -0500

    refactor program to functional layout so that testing can be written

--- react-cli.js
+++ react-cli.js
@@ -5,28 +5,13 @@ const path = require('path');
 const minimist = require('minimist');
 
 const cwd = process.cwd();
-let args;
 
-try {
-  args = minimist(process.argv.slice(2), {
-    boolean: ['functional', 'stateful', 'hooks', 'material', 'scss', 'css', 'modules', 'test', 'type', 'enzyme', 'help', 'version'],
-    alias: { h: 'hooks', f: 'functional', s: 'stateful', m: 'material', t: 'test', e: 'enzyme', v: 'version' },
-    '--': true,
-    stopEarly: false,
-    unknown: function (param) {
-      if (param[0] === '-') {
-        throw "Unknown param: '" + param + "' passed to rgen.";
-        return false;
-      }
-      return true;
-    }
-  });
-} catch (e) {
-  console.log(e);
-  return;
+function checkHelp(args) {
+  return args.help === true;
 }
 
-const helperText = `
+function printHelp() {
+  return `
 This utility will create a component for you.
 Usage:
     rgen [myComponent || my-component || MyComponent] ./path/to/parent/
@@ -49,18 +34,17 @@ Options:
     --scss and --modules together will create a .module.scss file instead.
     If no path is supplied, it will default to ./src/
 `;
-
-if (args.help) {
-  console.log(helperText);
-  return;
 }
 
-if (args.version) {
+function checkVersion(args) {
+  return args.version === true;
+}
+
+function printVersion() {
   const version = require('./package.json').version;
   const mostRecent = require('child_process').execSync("npm view gencom version").toString();
-  console.log(`    Installed: ${version}`);
-  console.log(`    Latest: ${mostRecent}`);
-  return;
+  return `    Installed: ${version}
+    Latest: ${mostRecent}`;
 }
 
 function upperCamelCase(string) {
@@ -76,34 +60,52 @@ function getDirectories(srcpath) {
     .filter(path => fs.statSync(path).isDirectory());
 }
 
-let [name, parentPath = './src/'] = args._;
-
-if ((args.functional && args.stateful) || (args.functional && args.hooks) || (args.stateful && args.hooks)) {
-  console.log(`Please use --functional, --stateful, and --hooks separately, exiting...`);
-  console.log(`Use -h or --help to see a list of options.`);
-  return;
+function checkTypeConflicts(args) {
+  if ((args.functional && args.stateful) || (args.functional && args.hooks) || (args.stateful && args.hooks)) {
+    return true;
+  }
+  return false;
 }
 
-if ((args.css && args.scss) || (args.css && args.material) || (args.scss && args.material)) {
-  console.log(`Please use --css, --scss, and --modules separately, exiting...`);
-  console.log(`Use -h or --help to see a list of options.`);
-  return;
+function typeConflicts() {
+  return `Please use --functional, --stateful, and --hooks separately, exiting...
+Use -h or --help to see a list of options.`
 }
 
-if (!name) {
-  console.log('A component name must be supplied, exiting...');
-  console.log(`Use -h or --help for help.`);
-  return;
+function checkStyleConflicts(args) {
+  if ((args.css && args.scss) || (args.css && args.material) || (args.scss && args.material)) {
+    return true;
+  }
+  return false;
 }
 
-name = upperCamelCase(name);
-let content;
-let testText;
+function styleConflicts() {
+  return `Please use --css, --scss, and --modules separately, exiting...
+Use -h or --help to see a list of options.`;
+}
 
-if (args.enzyme) {
+function checkMissingName(name) {
+  return typeof(name) === 'undefined';
+}
+
+function missingName() {
+  return `A component name must be supplied, exiting...
+Use -h or --help for help.`;
+}
+
+function checkTest(args) {
+  if (args.test || args.enzyme) {
+    return true;
+  }
+  return false;
+}
+
+function createTest(args, name) {
+  let testText = '';
   const lowerName = name[0].toLowerCase() + name.slice(1);
-  if (args.type) {
-    testText = `import React from 'react';
+  if (args.enzyme) {
+    if (args.type) {
+      testText += `import React from 'react';
 import { mount } from 'enzyme';
 import ${name} from './${name}';
 
@@ -134,8 +136,8 @@ describe("${name}", () => {
   });
 
 })`
-  } else {
-    testText = `import React from 'react';
+    } else {
+      testText += `import React from 'react';
 import { mount } from 'enzyme';
 import ${name} from './${name}';
 
@@ -166,102 +168,117 @@ describe("${name}", () => {
   });
 
 })`
-  }
-} else {
-  testText = `import React from 'react';
-  import ReactDOM from 'react-dom';
-  import ${name} from './${name}';
-  
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<${name} />, div);
-  });`
-}
-
-if (args.type && args.hooks) {
-  content = `import React, { FunctionComponent, useState, useEffect } from 'react';
-`;
-} else if (args.hooks) {
-  content = `import React, { useState, useEffect } from 'react';
-`;
-} else if (args.stateful) {
-  content = `import React, { Component } from 'react';
-`;
-} else {
-  content = `import React, { FunctionComponent } from 'react';
-`;
-}
-
-if (args.modules && args.scss) {
-  content += `import classes from './${name}.module.scss';
-`;
-} else if (args.modules) {
-  content += `import classes from './${name}.module.css';
-`;
-} else if (args.css) {
-  content += `import './${name}.css'
-`;
-} else if (args.scss) {
-  content += `import './${name}.scss'
-`;
-} else if (args.material) {
-  if (args.type) {
-    content += `import { withStyles, createStyles, WithStyles, Theme } from '@material-ui/core/styles';
-`;
+    }
   } else {
-    content += `import { withStyles } from '@material-ui/core/styles';
-`;
+    testText += `import React from 'react';
+import ReactDOM from 'react-dom';
+import ${name} from './${name}';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<${name} />, div);
+});`
   }
+  return testText;
 }
 
-content += `
+function createImports(args, name) {
+  let content = '';
+  if (args.type && args.hooks) {
+    content += `import React, { FunctionComponent, useState, useEffect } from 'react';
 `;
-
-if (args.material) {
-  if (args.type) {
-    content += `const styles = (theme: Theme) => createStyles({
-
-});
-
-`;
-  } else {
-    content += `const styles = theme => ({
-
-});
-
-`;
-  }
-}
-
-if (args.type) {
-  if (args.material) {
-    content += `interface Props extends WithStyles<typeof styles> {}
-
+  } else if (args.hooks) {
+    content += `import React, { useState, useEffect } from 'react';
 `;
   } else if (args.stateful) {
-    content += `interface Props {}
-
+    content += `import React, { Component } from 'react';
 `;
   } else {
-    content += `interface Props {}
+    content += `import React, { FunctionComponent } from 'react';
+`;
+  }
+
+  if (args.modules && args.scss) {
+    content += `import classes from './${name}.module.scss';
+`;
+  } else if (args.modules) {
+    content += `import classes from './${name}.module.css';
+`;
+  } else if (args.css) {
+    content += `import './${name}.css'
+`;
+  } else if (args.scss) {
+    content += `import './${name}.scss'
+`;
+  } else if (args.material) {
+    if (args.type) {
+      content += `import { withStyles, createStyles, WithStyles, Theme } from '@material-ui/core/styles';
+`;
+    } else {
+      content += `import { withStyles } from '@material-ui/core/styles';
+`;
+    }
+  }
+  content += `
+`;
+  return content;
+}
+
+function createMaterialStyles(args) {
+  let content = '';
+  if (args.material) {
+    if (args.type) {
+      content += `const styles = (theme: Theme) => createStyles({
+  
+});
+
+`;
+    } else {
+      content += `const styles = theme => ({
+  
+});
+
+`;
+    }
+  }
+  return content;
+}
+
+function createInterfaces(args) {
+  let content = '';
+  if (args.type) {
+    if (args.material) {
+      content += `interface Props extends WithStyles<typeof styles> {}
+
+`;
+    } else if (args.stateful) {
+      content += `interface Props {}
+
+`;
+    } else {
+      content += `interface Props {}
+
+`;
+    }
+  }
+  
+  if (args.type && args.stateful) {
+    content += `interface State {}
 
 `;
   }
+  return content;
 }
 
-if (args.type && args.stateful) {
-  content += `interface State {}
-  
-`;
-}
-
-if (args.type) {
-  if (args.stateful) {
-    content += `class ${name} extends Component<Props, State> {
+function createBody(args, name) {
+  let content = '';
+  if (args.type) {
+    if (args.stateful) {
+      content += `class ${name} extends Component<Props, State> {
   constructor(props: Props) {
     super(props);
     this.state: State = {
-
+      
     }
   }
 
@@ -269,15 +286,15 @@ if (args.type) {
     ${ args.material ? `const { classes } = this.props;` : ''}
     return (
       <>
-
+        
       </>
     );
   }
 }
-
+  
 `;
-  } else if (args.hooks) {
-    content += `const ${name}: FunctionComponent<Props> = (props: Props) => {
+    } else if (args.hooks) {
+      content += `const ${name}: FunctionComponent<Props> = (props: Props) => {
   ${ args.material ? `const { classes } = this.props;` : ''}
 
   const [data, setData] = useState('');
@@ -297,10 +314,10 @@ if (args.type) {
     </>
   );
 }
-
+  
 `;
-  } else {
-    content += `const ${name}: FunctionComponent<Props> = (props: Props) => {
+    } else {
+      content += `const ${name}: FunctionComponent<Props> = (props: Props) => {
   ${ args.material ? `const { classes } = this.props;` : ''}
 
   return (
@@ -311,10 +328,10 @@ if (args.type) {
 }
 
 `;
-  }
-} else {
-  if (args.stateful) {
-    content += `class ${name} extends Component {
+    }
+  } else {
+    if (args.stateful) {
+      content += `class ${name} extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -331,10 +348,10 @@ if (args.type) {
     );
   }
 }
-
+  
 `;
-  } else if (args.hooks) {
-    content += `const ${name} = (props) => {
+    } else if (args.hooks) {
+      content += `const ${name} = (props) => {
   ${ args.material ? `const { classes } = this.props;` : ''}
 
   const [data, setData] = useState('');
@@ -356,8 +373,8 @@ if (args.type) {
 }
 
 `;
-  } else {
-    content += `const ${name} = (props) => {
+    } else {
+      content += `const ${name} = (props) => {
   ${ args.material ? `const { classes } = this.props;` : ''}
 
   return (
@@ -368,52 +385,155 @@ if (args.type) {
 }
 
 `;
+    }
   }
+  return content;
 }
 
-if (args.material) {
-  content += `export default withStyles(styles)(${name});`
-} else {
-  content += `export default ${name};`
-}
-
-if (!fs.existsSync('./src/')) {
-  console.log(`Found no 'src' folder...`);
-  return;
-}
-
-const parentExists = fs.existsSync(parentPath);
-if (!parentExists) {
-  console.log(`Could not resolve provided path: ${parentPath}`);
-  return;
-}
-
-try {
-  fs.mkdirSync(`${parentPath}/${name}`);
-} catch (e) {
-  console.log(`A component with that name already exists, exiting...`);
-  return;
-}
-
-if (args.type) {
-  fs.writeFileSync(`${parentPath}/${name}/${name}.tsx`, content);
-} else {
-  fs.writeFileSync(`${parentPath}/${name}/${name}.js`, content);
-}
-if (args.test || args.enzyme) {
-  if (args.type) {
-    fs.writeFileSync(`${parentPath}/${name}/${name}.test.tsx`, testText);
+function createExport(args, name) {
+  let content = '';
+  if (args.material) {
+    content += `export default withStyles(styles)(${name});`
   } else {
-    fs.writeFileSync(`${parentPath}/${name}/${name}.test.js`, testText);
+    content += `export default ${name};`
+  }
+  return content;
+}
+
+function checkSourceFolder() {
+  if (!fs.existsSync('./src/')) {
+    return true;
+  }
+  return false;
+}
+
+function printNoSourceFolder() {
+  return `Found no 'src' folder, exiting...`;
+}
+
+function checkParentPath(parentPath) {
+  const parentExists = fs.existsSync(parentPath);
+  if (!parentExists) {
+    return;
   }
 }
-if (args.modules && args.scss) {
-  fs.writeFileSync(`${parentPath}/${name}/${name}.module.scss`, '');
-} else if (args.modules) {
-  fs.writeFileSync(`${parentPath}/${name}/${name}.module.css`, '');
-} else if (args.scss) {
-  fs.writeFileSync(`${parentPath}/${name}/${name}.scss`, '');
-} else if (args.css) {
-  fs.writeFileSync(`${parentPath}/${name}/${name}.css`, '');
+
+function printBadParentPath(parentPath) {
+  return `Could not resolve provided path: ${parentPath}`;
 }
-console.log(`${name} component has been created! Happy Coding!`)
+
+function makeFolder(parentPath, name) {
+  try {
+    fs.mkdirSync(`${parentPath}/${name}`);
+  } catch (e) {
+    return `A component with that name already exists, exiting...`;
+  }
+}
+
+function makeFiles(args, name, parentPath, content, testText) {
+  if (args.type) {
+    fs.writeFileSync(`${parentPath}/${name}/${name}.tsx`, content);
+  } else {
+    fs.writeFileSync(`${parentPath}/${name}/${name}.js`, content);
+  }
+  if (args.test || args.enzyme) {
+    if (args.type) {
+      fs.writeFileSync(`${parentPath}/${name}/${name}.test.tsx`, testText);
+    } else {
+      fs.writeFileSync(`${parentPath}/${name}/${name}.test.js`, testText);
+    }
+  }
+  if (args.modules && args.scss) {
+    fs.writeFileSync(`${parentPath}/${name}/${name}.module.scss`, '');
+  } else if (args.modules) {
+    fs.writeFileSync(`${parentPath}/${name}/${name}.module.css`, '');
+  } else if (args.scss) {
+    fs.writeFileSync(`${parentPath}/${name}/${name}.scss`, '');
+  } else if (args.css) {
+    fs.writeFileSync(`${parentPath}/${name}/${name}.css`, '');
+  }
+}
+
+function checkProblems(args) {
+  if (!args) {
+    return true;
+  }
+  if (checkHelp(args)) {
+    return printHelp();
+  }
+  if (checkVersion(args)) {
+    return printVersion();
+  }
+  if (checkSourceFolder()) {
+    return printNoSourceFolder();
+  }
+  if (checkMissingName(args)) {
+    return missingName();
+  }
+  if (checkStyleConflicts(args)) {
+    return styleConflicts();
+  }
+  if (checkTypeConflicts(args)) {
+    return typeConflicts();
+  }
+  if (checkParentPath(args)) {
+    return printBadParentPath();
+  }
+  return false;
+}
+
+function getArgs() {
+  let args;
+  try {
+    args = minimist(process.argv.slice(2), {
+      boolean: ['functional', 'stateful', 'hooks', 'material', 'scss', 'css', 'modules', 'test', 'type', 'enzyme', 'help', 'version'],
+      alias: { h: 'hooks', f: 'functional', s: 'stateful', m: 'material', t: 'test', e: 'enzyme', v: 'version' },
+      '--': true,
+      stopEarly: false,
+      unknown: function (param) {
+        if (param[0] === '-') {
+          throw "Unknown param: '" + param + "' passed to rgen.";
+          return false;
+        }
+        return true;
+      }
+    });
+  } catch (e) {
+    return { error: e }
+  }
+  return args;
+}
+
+function main() {
+  let args = getArgs();
+  if (args.error) {
+    console.log(args.error);
+    return;
+  }
+  let problem = checkProblems(args);
+  if (problem) {
+    console.log(problem);
+    return;
+  }
+  const [name, parentPath = './src/'] = args._;
+  const fixedName = upperCamelCase(name);
+  let content = '';
+  let testText = createTest(args, fixedName);
+  if (checkTest(args)) {
+    testText = createTest(args, fixedName);
+  }
+  content += createImports(args, fixedName);
+  content += createMaterialStyles(args);
+  content += createInterfaces(args);
+  content += createBody(args, fixedName);
+  content += createExport(args, fixedName);
+  const folderProblem = makeFolder(parentPath, fixedName);
+  if (folderProblem) {
+    console.log(folderProblem);
+    return;
+  }
+  makeFiles(args, fixedName, parentPath, content, testText);
+  console.log(`${name} component has been created! Happy Coding!`)
+}
+
+main();
